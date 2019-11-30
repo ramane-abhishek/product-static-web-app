e3971f7f72ef4be425162d7eb06b6a75950d209c
ramane-abhishek <ramaneabhishek.29@gmail.com>
2019-11-30 23:10:28 +0530

    Added Product page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li style={{ display: `inline-block`, marginRight: `1rem`}}>
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
@@ -10,10 +10,11 @@ export default ({ children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: '80%', padding: `0 1rem` }}>
     <header style={{ marginBottom: `1.5rem` }}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+        <h3 style={{ display: `inline` }}>Material Handling</h3>
       </Link>
       <ul style={{ listStyle: `none`, float: `right` }}>
         <ListLink to="/">Home</ListLink>
+        <ListLink to="/products">Products</ListLink>
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
       </ul>
