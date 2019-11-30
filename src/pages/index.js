e0773c09fc412313e70556147df8ddff0d474ad4
ramane-abhishek <ramaneabhishek.29@gmail.com>
2019-11-30 20:24:43 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Hi! I'm building a Site as for listing all the products</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
+)
