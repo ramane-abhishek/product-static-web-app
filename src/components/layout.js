615a6caffd3a93d2045552564625354d8e77bf9e
abhishek-ramane <abhishek-ramane@github.com>
2019-12-21 18:55:41 +0530

    Added responsive user interface with tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,45 @@
 import React from "react"
+import containerStyles from "./layout.css"
 import { Link } from "gatsby"
-const ListLink = props => (
-  <li>
-    <Link
-      className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
-      to={props.to}
-    >
-      {props.children}
-    </Link>
-  </li>
-)
 
 export default ({ children }) => (
-  <div className="container font-sans w-full px-6 lg:px-16 ">
-    <header className="flex flex-wrap items-center lg:py-0 py-2">
-      <div className="flex-1 flex justify-between items-center">
-        <Link to="/">
-          <h3>Material Handling</h3>
-        </Link>
-      </div>
+  <div className={containerStyles.container}>
+    <div className="container w-full px-6 lg:px-16 ">
+      <header className="flex flex-wrap items-center lg:py-0 py-2">
+      <nav id="header" class="w-full z-30 top-0 py-1">
+            <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
+                <label for="menu-toggle" class="cursor-pointer md:hidden block">
+                    <svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                        <title>menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                    </svg>
+                </label>
+                <input class="hidden" type="checkbox" id="menu-toggle" />
 
-      <label for="menu-toggle" className="pointer-cursor lg:hidden block">
-        <svg
-          className="fill-current text-gray-900"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-        >
-          <title>menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-        </svg>
-      </label>
-      <input className="hidden" type="checkbox" id="menu-toggle" />
+                <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
+                    <nav>
+                        <ul class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+                            <li><Link class="inline-block no-underline hover:text-black hover:underline py-2 px-4" to="/products">Products</Link></li>
+                            <li><Link class="inline-block no-underline hover:text-black hover:underline py-2 px-4" to="/about">About</Link></li>
+                            <li><Link class="inline-block no-underline hover:text-black hover:underline py-2 px-4" to="/contact">Contact</Link></li>
 
-      <div
-        className="hidden lg:flex lg:items-center lg:w-auto w-full"
-        id="menu"
-      >
-        <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-          <ListLink to="/">Home</ListLink>
-          <ListLink to="/products">Products</ListLink>
-          <ListLink to="/about/">About</ListLink>
-          <ListLink to="/contact/">Contact</ListLink>
-        </ul>
-      </div>
-    </header>
-    <div className="container">{children}</div>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="order-1 md:order-2">
+                  <Link class="flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " to="/">
+                    <svg class="fill-current text-gray-800 mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path d="M5,22h14c1.103,0,2-0.897,2-2V9c0-0.553-0.447-1-1-1h-3V7c0-2.757-2.243-5-5-5S7,4.243,7,7v1H4C3.447,8,3,8.447,3,9v11 C3,21.103,3.897,22,5,22z M9,7c0-1.654,1.346-3,3-3s3,1.346,3,3v1H9V7z M5,10h2v2h2v-2h6v2h2v-2h2l0.002,10H5V10z" />
+                    </svg>
+                  SOMENAME
+                  </Link>
+                </div>  
+
+            </div>
+        </nav>
+      </header>
+      <div className="container">{children}</div>
+    </div>
   </div>
 )
