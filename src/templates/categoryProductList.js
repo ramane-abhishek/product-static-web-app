7f36ece7f67d31ab8492143a27bf50553f828cac
abhishek-ramane <abhishek-ramane@github.com>
2020-01-08 22:09:34 +0530

    Added code for listing products of specific category

--- src/templates/categoryProductList.js
+++ src/templates/categoryProductList.js
@@ -1,44 +1,62 @@
- import React from "react"
- import { graphql } from "gatsby"
- import * as PropTypes from "prop-types"
- import Layout from "../components/layout"
- const propTypes = {
-   data: PropTypes.object.isRequired,
- }
- class CategoryTemplate extends React.Component {
-   render() {
-     const allProduct = this.props.data.allContentfulProduct
-     console.log(allProduct)
-     return (
-       <Layout>
-         {/* <div className="sticky top-0"> */}
-         <div>Something</div>
-       </Layout>
-     )
-   }
- }
- CategoryTemplate.propTypes = propTypes
- export default CategoryTemplate
- export const pageQuery = graphql`
-    query($categoryId: String) {
-        allContentfulProduct(filter: {categoryId: {elemMatch: {id: {eq: $categoryId}}}}) {
-          edges {
-            node {
-              id
-              categoryId {
-                id
-              }
-              productId
-              productName
-              slug
-              image {
-                file {
-                  url
-                }
-              }
+import React from "react"
+import { Link, graphql } from "gatsby"
+import * as PropTypes from "prop-types"
+import Layout from "../components/layout"
+const propTypes = {
+  data: PropTypes.object.isRequired,
+}
+class CategoryTemplate extends React.Component {
+  render() {
+    const allProduct = this.props.data.allContentfulProduct.edges
+    console.log(allProduct)
+    return (
+      <Layout>
+        {/* <div className="sticky top-0"> */}
+        <div>Something</div>
+        <div className="flex flex-wrap">
+          {allProduct.map(({ node: product }) => (
+            <Link to={`/products/${product.slug}`}>
+              <div key={product.id} className="px-2">
+                <div
+                  key={product.id}
+                  className="w-full bg-gray-500 px-2 py-2 mx-2 my-2"
+                >
+                  <div>
+                    {product.categoryName}
+                    <h3>{product.id}</h3>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </Layout>
+    )
+  }
+}
+CategoryTemplate.propTypes = propTypes
+export default CategoryTemplate
+export const pageQuery = graphql`
+  query($categoryId: String) {
+    allContentfulProduct(
+      filter: { categoryId: { elemMatch: { id: { eq: $categoryId } } } }
+    ) {
+      edges {
+        node {
+          id
+          categoryId {
+            id
+          }
+          productId
+          productName
+          slug
+          image {
+            file {
+              url
             }
           }
         }
       }
-      
- `
+    }
+  }
+`
